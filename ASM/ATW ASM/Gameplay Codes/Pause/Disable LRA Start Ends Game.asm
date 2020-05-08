c37c3bb77a639414cc88af7c1a7a6381cf380c7d
BiigMoney <64580434+BiigMoney@users.noreply.github.com>
2020-05-08 11:23:10 -0700

    fixed dumb glitch + other stuff

--- ASM/ATW ASM/Gameplay Codes/Pause/Disable LRA Start Ends Game.asm
+++ ASM/ATW ASM/Gameplay Codes/Pause/Disable LRA Start Ends Game.asm
@@ -1,3 +1,14 @@
 #To be inserted at 8016D1C0
+
+.include "Common/common.s"
+mr r20,r3
+branchl r12,countPlayersInMatch
+cmpwi r3,1
+bgt NO
+mr r3,r20
+branchl r12,0x801a36a0
+b THERE
+NO:
 li r3,0
 li r4,0
+THERE:
